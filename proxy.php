a2683483ae74e01a9a5c92cd7c4ab8483914bf4d
sapphirediamondcasino-bit <sapphirediamondcasino@gmail.com>
2025-11-12 14:05:02 -0600

    Create proxy.php

---
+++ proxy.php
@@ -0,0 +1,34 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
+header('Content-Type: application/json');
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
+
+$apiBase = 'http://193.31.31.105:25723';
+$path = isset($_GET['path']) ? $_GET['path'] : '/api/feed';
+$url = $apiBase . $path;
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $postData = file_get_contents('php://input');
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+}
+
+$response = curl_exec($ch);
+$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+curl_close($ch);
+
+http_response_code($httpCode);
+echo $response;
+?>
